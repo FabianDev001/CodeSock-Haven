4b13169ecce783e0e2563d43937bee351732e296
FabianDev001 <farbiandevelop@gmail.com>
2023-11-30 23:18:24 +0100

    🚀💾 Added products.ts
    Here are all products I want to list

---
+++ src/products.ts
@@ -0,0 +1,32 @@
+const products = [
+    {
+      id: 1,
+      name: "Nice green Coding Socks",
+      price: "$19.99",
+      imageUrl: "/images/ProgrammingSocks1.jpg",
+      smallDescription: "2 PACK x Socks for Programmers",
+      largeDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl sed tincidunt aliquam, nunc nibh aliquet nunc, vitae aliquam",
+      rating: 3,
+    },
+    {
+      id: 2,
+      name: "Big blue Coding Socks",
+      price: "$24.99",
+      imageUrl: "/images/ProgrammingSocks2.jpg",
+      smallDescription: "2 PACK x Socks for Programmers",
+      largeDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl sed tincidunt aliquam, nunc nibh aliquet nunc, vitae aliquam",
+      rating: 1,
+    },
+    {
+      id: 3,
+      name: "Buggy Coding Socks",
+      price: "$19.99",
+      imageUrl: "/images/ProgrammingSocks3.jpg",
+      smallDescription: "2 PACK x Socks for Programmers",
+      largeDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl sed tincidunt aliquam, nunc nibh aliquet nunc, vitae aliquam",
+      rating: 4,
+    },
+    
+  ];
+
+  export default products;
